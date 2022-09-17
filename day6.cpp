cb620d0c9752e511c4df4cc4091609d45500ac1a
Pratyaksha Newalkar <newalkarpratyaksha@gmail.com>
2022-09-17 15:52:36 +0530

    Create day6.cpp

---
+++ day6.cpp
@@ -0,0 +1,19 @@
+//Codechef Problem:Chefland Games
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        int a,b,c,d;
+        cin>>a>>b>>c>>d;
+        if(a||b||c||d)
+    cout<<"\nOUT";
+        else
+        cout<<"\nIN";
+    }
+    return 0;
+}
